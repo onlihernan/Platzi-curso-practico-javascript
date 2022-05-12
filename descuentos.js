f69279269e8f179653faaad63e8f686ced91843d
hernan desktop <onlihernan@hotmail.com>
2022-05-12 01:14:41 -0300

    Se creo un nuevo proyecto de precios y descuentos

---
+++ descuentos.js
@@ -0,0 +1,32 @@
+const precioOriginal = 100;
+const descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount () {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const priceDiscount = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, priceDiscount);
+    
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
+
+
+
+// console.log({
+//    precioOriginal,
+//    descuento,
+//    porcentajePrecioConDescuento,
+//    precioConDescuento, 
+// });
+
+ 

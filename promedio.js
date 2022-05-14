e4765b3ab606e44a303d90745f53d215f0548ec9
hernan desktop <onlihernan@hotmail.com>
2022-05-14 16:37:30 -0300

    actualización de los archivos de promedio

--- promedio.js
+++ promedio.js
@@ -1,11 +1,41 @@
-const lista1 = [
-    100,
-    200,
-    300,
-    500,
-];
+// CALCULO DEL PROMEDIO DE UNA LISTA CON VALORES DETERMINADOS DE UN ARRAY:
 
-let sumaLista1 = 0;
-for(let i = 0; i < lista1.lenght, i++){
-    sumaLista1 = sumaLista1 + lista1[i];
+
+// const lista1 = [
+//     100,
+//     200,
+//     300,
+//     500
+// ];
+
+// let sumaLista1 = 0;
+
+// for (let i = 0; i < lista1.length; i++) {  ---> let i = 0 indica el comienzo de del array / i < lista1.length indica que el recuento se detendra cuando los elementos de i hayan alcanzado el valor anterior al total de elementos existentes / i++ indica que al final del ciclo se sumara un elemento
+//     sumaLista1 = sumaLista1 + lista1[i];   ---> sumaLista1 es igual 
+// }
+
+
+
+
+// const promedioLista1 = sumaLista1 / lista1.length;
+
+
+// CALCULO DE UNA LISTA CON UNA FUNCION:
+
+function calcularMediaAritmetica(lista) {
+    // let sumaLista = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //     sumaLista = sumaLista + lista[i];
+    // }
+
+    const sumaLista = lista.reduce(
+         function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+         }
+    );
+
+    
+    const promedioLista = sumaLista / lista.length; 
+    return promedioLista;
+    
 }
